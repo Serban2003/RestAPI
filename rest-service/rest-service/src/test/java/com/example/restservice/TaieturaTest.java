518574dba6852977e68e4f8b45bca038c0a530d5
Serban2003 <46806049+Serban2003@users.noreply.github.com>
2020-03-30 11:44:35 +0300

    Add TaieturaTest

---
+++ rest-service/rest-service/src/test/java/com/example/restservice/TaieturaTest.java
@@ -0,0 +1,27 @@
+package com.example.restservice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class TaieturaTest {
+
+    private Taietura taietura = new Taietura();
+
+    @DisplayName("Should not throw an exception")
+
+    @Test
+    void testall() {
+        double time_before = System.nanoTime();
+
+        int[] v = {2, -2, 0, 0, 1, -1};
+        int[] expected_array = {4, 4, 6, 6, 4, 4};
+
+        assertArrayEquals(expected_array, taietura.taieturi(v));
+
+        double time_after = System.nanoTime();
+
+        System.out.println((time_after - time_before) / 1_000_000);
+    }
+}
