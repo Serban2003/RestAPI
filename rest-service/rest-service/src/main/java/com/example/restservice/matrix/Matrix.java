ca344df381d3873cfbb9499fd6dfc5b51a9b910b
Serban <iustinianserban@gmail.com>
2020-11-07 17:01:17 +0200

    create Matrix class and implement add, subtract, product and transpose functions

---
+++ rest-service/rest-service/src/main/java/com/example/restservice/matrix/Matrix.java
@@ -0,0 +1,91 @@
+package com.example.restservice.matrix;
+
+import java.util.Random;
+
+public class Matrix {
+    double[][] mat;
+    int n, m;
+
+    public Matrix(int n, int m){
+        this.n = n;
+        this.m = m;
+        mat = new double[n][m];
+    }
+
+    public Matrix(double v[][]){
+        n = v.length;
+        m = v[0].length;
+        mat = new double[n][m];
+
+        for(int i = 0; i < n; ++i)
+            System.arraycopy(v[i], 0, mat[i], 0, v[i].length);
+    }
+
+    public Matrix add(Matrix matrix){
+        if(n != matrix.n && m != matrix.m) throw new IllegalArgumentException("The matrices don't have the same size");
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                mat[i][j] += matrix.mat[i][j];
+
+        return this;
+    }
+
+    public Matrix subtract(Matrix matrix){
+        if(n != matrix.n && m != matrix.m) throw new IllegalArgumentException("The matrices don't have the same size");
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                mat[i][j] -= matrix.mat[i][j];
+
+        return this;
+    }
+
+    public Matrix product(Matrix matrix){
+        if(n != matrix.m && m != matrix.n) throw new IllegalArgumentException("The matrices don't have the same size");
+        matrix.transpose();
+
+        Matrix result = new Matrix(n, m);
+
+        for(int i = 0; i < n; ++i){
+            int sum = 0;
+            for(int t = 0; t < m; ++t){
+                sum = 0;
+                for(int j = 0; j < m; ++j)
+                    sum += mat[i][j] * matrix.mat[i][j];
+                result.mat[i][t] = sum;
+            }
+        }
+        return result;
+    }
+
+    public Matrix transpose(){
+        double[][] matCopy = this.mat;
+        this.mat = new double[m][n];
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j)
+                this.mat[i][j] = matCopy[j][i];
+        int aux = n;
+        n = m;
+        m = aux;
+
+        return this;
+    }
+
+    public Matrix generate(int n, int m, int min, int max){
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                mat[i][j] = min + (Math.random() * (max - min + 1));
+
+        return this;
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j)
+                builder.append(mat[i][j]).append(" ");
+            builder.append('\n');
+        }
+        return builder.toString();
+    }
+}
---
+++ rest-service/rest-service/src/test/java/com/example/restservice/matrix/MatrixTest.java
@@ -0,0 +1,66 @@
+package com.example.restservice.matrix;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatrixTest {
+
+    @Test
+    public void testToString(){
+        assertEquals("1.0 2.0 \n" + "2.0 3.0 \n", new Matrix(new double[][] {{1, 2}, {2, 3}}).toString());
+    }
+    @Test
+    public void testInitialization(){
+        assertEquals("0.0 0.0 \n" + "0.0 0.0 \n", new Matrix(2,2).toString());
+    }
+
+    @Test
+    public void testAddMatrix(){
+        Matrix matrix1 = new Matrix(new double[][]{{2, 5}, {4, 5}});
+        Matrix matrix2 = new Matrix(new double[][]{{3, 7},{6, 9}});
+        assertEquals("5.0 12.0 \n" + "10.0 14.0 \n", matrix1.add(matrix2).toString());
+    }
+    @Test
+    public void testSubtractMatrix(){
+        Matrix matrix1 = new Matrix(new double[][]{{2, 5}, {4, 5}});
+        Matrix matrix2 = new Matrix(new double[][]{{3, 7},{6, 9}});
+        assertEquals("-1.0 -2.0 \n" + "-2.0 -4.0 \n", matrix1.subtract(matrix2).toString());
+    }
+    @Test
+    public void testTranspose(){
+        Matrix matrix = new Matrix(new double[][]{{2, 5, 3}, {4, 5, 9}});
+        assertEquals("2.0 4.0 \n" + "5.0 5.0 \n" + "3.0 9.0 \n", matrix.transpose().toString());
+    }
+
+    @Test
+    public void testProductMatrix(){
+        Matrix matrix1 = new Matrix(new double[][]{{2, 5}});
+        Matrix matrix2 = new Matrix(new double[][]{{3}, {6}});
+        assertEquals("36.0 36.0 \n", matrix1.product(matrix2).toString());
+    }
+
+    @Test
+    public void testComplexity(){
+
+
+        for(int i = 1; i <= 150; ++i){
+
+            double totalTime = 0.0;
+            for(int j = 1; j <= 10; ++j){
+                Matrix matrix1 = new Matrix(i, i);
+                Matrix matrix2 = new Matrix(i, i);
+
+                matrix1.generate(i, i, -100, 100);
+                matrix2.generate(i, i, -100, 100);
+
+                double timeBefore = System.nanoTime();
+
+                matrix1.product(matrix2);
+
+                totalTime += (System.nanoTime() - timeBefore);
+            }
+            System.out.println("Multiplying matrices of size " + i + ": " + totalTime / 10 / 1_000_000 + " ms.");
+        }
+    }
+}
